19f780b315544ed7a698a2f0c4e80a4438d6e88d
Max Simpson <maxsimpson95@gmail.com>
2020-06-29 07:30:01 +0800

    linter, prettify and travis setup

--- react-employee/src/faker.js
+++ react-employee/src/faker.js
@@ -1,19 +1,19 @@
-var faker = require('faker');
+var faker = require('faker')
 
-let departments = []
+const departments = []
 
 for (let index = 0; index < numberOfDepartments; index++) {
-    const randomDepartment = faker.commerce.department()
+  const randomDepartment = faker.commerce.department()
 }
 
-let employees = []
+const employees = []
 
 for (let index = 0; index < numberOfEmployees; index++) {
-    const randomName = faker.name.findName(); // Rowan Nikolaus
-    const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
-    const randomDepartment = faker.commerce.department()
+  const randomName = faker.name.findName() // Rowan Nikolaus
+  const randomEmail = faker.internet.email() // Kassandra.Haley@erich.biz
+  const randomDepartment = faker.commerce.department()
 }
 
 // var randomName = faker.name.findName(); // Rowan Nikolaus
 // var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
-// var randomCard = faker.helpers.createCard(); // random contact 
+// var randomCard = faker.helpers.createCard(); // random contact
