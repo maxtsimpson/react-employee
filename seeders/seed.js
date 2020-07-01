3b542069ca88f47b0c31b4dfbca0efacd636bfbb
Max Simpson <maxsimpson95@gmail.com>
2020-07-01 14:44:26 +0800

    finished seed file

--- seeders/seed.js
+++ seeders/seed.js
@@ -6,11 +6,11 @@ const connection = require('../config/mongo')
 
 async function seed (numberOfDepartments, numberOfEmployees) {
   return new Promise((resolve, reject) => {
-    console.log('running seed')
     for (let index = 0; index < numberOfDepartments; index++) {
-      const department = { departmentName: faker.commerce.department() }
-      departments.push(department)
-      const employees = []
+      const department = {
+        departmentName: faker.commerce.department(),
+        employees: []
+      }
 
       for (let index = 0; index < numberOfEmployees; index++) {
         const employee = {
@@ -18,53 +18,32 @@ async function seed (numberOfDepartments, numberOfEmployees) {
           lastName: faker.name.lastName(),
           role: faker.name.jobTitle()
         }
-        employees.push(employee)
+        department.employees.push(employee)
       }
+      departments.push(department)
     }
 
     db.Department.deleteMany({})
-      .then(() => db.Department.insertMany(departments))
-      .then(data => {
-        resolve(data)
+      .then(() => {
+        db.Department.insertMany(departments)
+          .then(() => {
+            resolve()
+          })
       })
       .catch(err => {
         reject(err)
       })
   })
-
-  // db.Department.create({ departmentName: faker.commerce.department() })
-  //   .then((results) => {
-  //     console.log(results)
-  //   })
-  //   .catch(error => {
-  //     console.log('there was an error')
-  //     console.log(error)
-  //   })
 }
 
-// const employees = []
-
-// for (let index = 0; index < numberOfEmployees; index++) {
-
-//   const randomName = faker.name.findName() // Rowan Nikolaus
-//   const randomEmail = faker.internet.email() // Kassandra.Haley@erich.biz
-//   const randomDepartment = faker.commerce.department()
-// }
-
-// export default populate
-
-connection.then(() => {
-  db.Department.deleteMany({})
-    .then(async () => {
-      await seed(1, 5)
-    })
-    .then(data => {
-      console.table(data)
-      process.exit(0)
-    })
-    .catch(err => {
-      console.log(err)
-      process.exit(1)
-    })
-})
-  .catch(error => console.log(error))
+connection
+  .then(async () => {
+    await seed(3, 5)
+  })
+  .then(() => {
+    process.exit(0)
+  })
+  .catch(err => {
+    console.log(err)
+    process.exit(1)
+  })
