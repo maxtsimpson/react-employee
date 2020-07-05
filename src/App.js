70ab08b0c74bac22857c78f2d7eb18d1cd86893d
Max Simpson <maxsimpson95@gmail.com>
2020-07-05 21:34:57 +0800

    removed unused list component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import EmployeeList from "./components/EmployeeList"
-import List from "./components/List";
 import { generateEmployees } from "./utils/generateEmployees";
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
     <div className="container text-center">
         <h4>My Employee List:</h4>
         <EmployeeList employees={employees}/>
-        {/* <List employees={employees}/> */}
     </div>
   );
 }
